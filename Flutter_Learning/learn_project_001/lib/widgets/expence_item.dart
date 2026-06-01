87c496d854c61d36bd49dfee6283e1c7348cee83
SasanDilanthaSTD <sasandilantha12@gmail.com>
2026-06-01 20:15:30 +0530

    imp: create item tile

--- Flutter_Learning/learn_project_001/lib/widgets/expence_item.dart
+++ Flutter_Learning/learn_project_001/lib/widgets/expence_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learn_project_001/models/expence.dart';
+import 'package:learn_project_001/widgets/expence_tile.dart';
 
 class ExpenceListItems extends StatelessWidget {
   const ExpenceListItems({super.key, required this.list});
@@ -11,34 +12,7 @@ class ExpenceListItems extends StatelessWidget {
     return ListView.builder(
       itemCount: list.length,
       itemBuilder: (context, index) {
-        return Card(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  list[index].title,
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                const SizedBox(height: 8),
-                Row(
-                  children: [
-                    Text(list[index].amount.toStringAsFixed(2)),
-                    const Spacer(),
-                    Row(
-                      children: [
-                        const Icon(Icons.trending_down),
-                        const SizedBox(width: 10),
-                        Text(list[index].date.toString()),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        );
+        return ExpenceTile(expence: list[index]);
       },
     );
   }
---
+++ Flutter_Learning/learn_project_001/lib/widgets/expence_tile.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:learn_project_001/models/expence.dart';
+
+class ExpenceTile extends StatelessWidget {
+  const ExpenceTile({super.key, required this.expence});
+
+  final ExpenceModel expence;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(expence.title, style: Theme.of(context).textTheme.titleLarge),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                Text(expence.amount.toStringAsFixed(2)),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.trending_down),
+                    const SizedBox(width: 10),
+                    Text(expence.date.toString()),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
